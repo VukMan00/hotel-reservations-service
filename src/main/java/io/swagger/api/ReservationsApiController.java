1c6f52adf7b3eb2ba68a18d99ea8760dfee0de6a
vuk.manojlovic <vukman619@gmail.com>
2024-01-25 14:38:51 +0100

    Handled CORS configuration

--- src/main/java/io/swagger/api/ReservationsApiController.java
+++ src/main/java/io/swagger/api/ReservationsApiController.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-01-11T08:59:36.048365385Z[GMT]")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReservationsApiController implements ReservationsApi {
 
     private static final Logger log = LoggerFactory.getLogger(ReservationsApiController.class);
--- src/main/java/io/swagger/api/RoomsApiController.java
+++ src/main/java/io/swagger/api/RoomsApiController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,6 +19,7 @@ import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-01-11T08:59:36.048365385Z[GMT]")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class RoomsApiController implements RoomsApi {
 
     private static final Logger log = LoggerFactory.getLogger(RoomsApiController.class);
